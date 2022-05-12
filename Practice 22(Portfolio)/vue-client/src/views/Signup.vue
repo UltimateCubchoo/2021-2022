6827ca7ea8d82d3b6645c2c7b7011a330cc001aa
navymrc <navymrc@gmail.com>
2022-05-12 10:14:59 -0400

    Changing dog border

--- Practice 22(Portfolio)/vue-client/src/views/Signup.vue
+++ Practice 22(Portfolio)/vue-client/src/views/Signup.vue
@@ -6,7 +6,7 @@
                     <div class="col-md-10 col-lg-8 mx-auto text-center">
                         <i class="fa-solid fa-dog fa-2x mb-2 text-white"></i>
                         <h2 class="text-white mb-5">Look at my dog!</h2>
-                        <div>
+                        <div class="imgDiv">
                             <img src="https://pbs.twimg.com/media/FSayIaPXIAEoQd-?format=jpg&name=large">
                         </div>
                     </div>
@@ -16,12 +16,16 @@
 </template>
 
 <style scoped>
+    .imgDiv{
+        border-style:solid;
+        border-color: white;
+        background-image:linear-gradient(#ffffff, transparent) 1;
+        border-top-right-radius: 5rem;
+        border-bottom-left-radius: 5rem;
+        border-bottom:0;
+        overflow: hidden;
+    }
     img{
         width: 100%;
-        border-style:solid;
-        background-image:linear-gradient(#ffffff, transparent) ;
-        border-top-right-radius: 100px;
-        border-bottom-left-radius: 100px;
-        border-bottom:0;
     }
 </style>
