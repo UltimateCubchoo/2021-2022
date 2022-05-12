14f2c87b25c534aa1ed67fa66c1d87ce05feba45
navymrc <navymrc@gmail.com>
2022-05-12 10:01:35 -0400

    Made Portfolio

---
+++ Practice 22(Portfolio)/vue-client/src/App.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import {defineComponent} from "vue"
+import Navigation from "./components/Nav.vue"
+import Masthead from "./views/Masthead.vue"
+import About from "./views/About.vue"
+import Projects from "./views/Projects.vue"
+import Signup from "./views/Signup.vue"
+import Contact from "./views/Contact.vue"
+import Footer from "./components/Footer.vue"
+export default defineComponent({
+    components:{
+      Navigation: Navigation,
+      Masthead: Masthead,
+      About: About,
+      Contact: Contact, 
+      Projects: Projects,
+      Signup: Signup,
+      Footer: Footer
+    }
+});
+</script>
+
+<template>
+  <Navigation></Navigation>
+  <Masthead></Masthead>
+  <About></About>
+  <Projects></Projects>
+  <Signup></Signup>
+  <Contact></Contact>
+  <Footer></Footer>    
+</template>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ Practice 22(Portfolio)/vue-client/src/views/Signup.vue
@@ -0,0 +1,27 @@
+<template>
+    <!-- Signup-->
+        <section class="signup-section" id="signup">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-md-10 col-lg-8 mx-auto text-center">
+                        <i class="fa-solid fa-dog fa-2x mb-2 text-white"></i>
+                        <h2 class="text-white mb-5">Look at my dog!</h2>
+                        <div>
+                            <img src="https://pbs.twimg.com/media/FSayIaPXIAEoQd-?format=jpg&name=large">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+</template>
+
+<style scoped>
+    img{
+        width: 100%;
+        border-style:solid;
+        background-image:linear-gradient(#ffffff, transparent) ;
+        border-top-right-radius: 100px;
+        border-bottom-left-radius: 100px;
+        border-bottom:0;
+    }
+</style>
